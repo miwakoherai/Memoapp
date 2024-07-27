c7714506438c2927f685ffbb173e0e34aa9022bc
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-07-27 10:02:35 +0900

    circleButtonのスタイリング完成

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -39,8 +39,8 @@ const Index = (): JSX.Element => {
           </View>
         </View>
       </View>
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -91,19 +91,26 @@ const styles = StyleSheet.create({
     lineHeight: 16,
     color: "#848484",
   },
-  main: {
-    flex: 1,
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
     justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8,
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 39,
   },
 });
 export default Index;
