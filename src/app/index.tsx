841fb5b5bf5a6f223e0dad032a8dc2cd73902fc9
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-07-27 14:35:11 +0900

    list.tsxの作成と、　index.tsxのredirect設定

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,8 @@
-import { StyleSheet, View } from "react-native";
+import { Redirect } from "expo-router";
 import React from "react";
-import Header from "../components/Header";
-import MemoListItem from "../components/MemoListItem";
-import CircleButton from "../components/CircleButton";
 
 const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <CircleButton>+</CircleButton>
-    </View>
-  );
+  return <Redirect href="memo/list" />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-  },
-});
 export default Index;
