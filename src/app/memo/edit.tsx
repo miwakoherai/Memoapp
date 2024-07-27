e1889e33972c670c09c8d9b3ad5dde69cf628946
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-07-28 08:47:17 +0900

    momoの詳細画面の構造化とスタイリング

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,43 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import React from "react";
+
+import Header from "../../components/Header";
+import CircleButton from "../../components/CircleButton";
+import Icon from "../../components/Icon";
+
+const Edit = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter text here"
+          multiline
+          style={styles.input}
+          value={"買い物\nメモ"}
+        />
+      </View>
+      <CircleButton>
+        <Icon name="check" size={40} color="#ffffff" />
+      </CircleButton>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: "top",
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
+export default Edit;
