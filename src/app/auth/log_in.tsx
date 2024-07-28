ace6822f319a9b002d2fdd7de5f950bb0f365630
MiwakoHerai <jg.miwakoherai@gmail.com>
2024-07-28 16:58:25 +0900

    LogIn画面のマークアップ

---
+++ src/app/auth/log_in.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+
+import Header from "../../components/Header";
+
+const LogIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <Text>Log In</Text>
+        <TextInput value="Email address" />
+        <TextInput value="Password" />
+        <View>
+          <Text>Submit</Text>
+        </View>
+        <View>
+          <Text>Not registered?</Text>
+          <Text>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default LogIn;
